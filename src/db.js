2d746cddd3a7f6cc9da2a0c54a0cd2b248faf304
gudlaugagnes <gak16@hi.is>
2021-02-13 23:50:31 +0000

    lagfæring frá Ola í tilkynningu fyrir heroku

--- src/db.js
+++ src/db.js
@@ -7,7 +7,20 @@ dotenv.config();
 
 const {
   DATABASE_URL: connectionString,
+  NODE_ENV: nodeEnv = 'development',
 } = process.env;
+
+console.log('process.env :>> ', process.env.DATABASE_URL);
+
+if (!connectionString) {
+  console.error('Vantar DATABASE_URL!');
+  process.exit(1);
+}
+
+// Notum SSL tengingu við gagnagrunn ef við erum *ekki* í development mode, þ.e.a.s. á local vél
+const ssl = nodeEnv !== 'development' ? { rejectUnauthorized: false } : false;
+
+const pool = new pg.Pool({ connectionString, ssl });
 
 if (!connectionString) {
   console.error('Vantar DATABASE_URL');
